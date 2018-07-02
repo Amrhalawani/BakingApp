5b81cb029dbb089813fa901405e41873eb0161ef
Amrhal <eng.amrhalawani@rocketmail.com>
2018-07-02 09:05:09 +0200

    add checking variable for phone and tablet

--- app/src/main/java/com/nd/amrhal/bakingapp/DetailActivity.java
+++ app/src/main/java/com/nd/amrhal/bakingapp/DetailActivity.java
@@ -1,19 +1,23 @@
 package com.nd.amrhal.bakingapp;
 
-import android.app.Fragment;
+
+import android.content.SharedPreferences;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.util.Log;
+
 import android.widget.Toast;
 
 import com.nd.amrhal.bakingapp.Ingredient.IngredientModel;
 import com.nd.amrhal.bakingapp.Recipes.RecipeModel;
 
+
 import java.util.List;
 
-public class DetailActivity extends AppCompatActivity implements RecipeDetailFragment.SendMessage{
+import com.nd.amrhal.bakingapp.Keys;
+
+public class DetailActivity extends AppCompatActivity implements RecipeDetailFragment.SendMessage {
 
     final static String INGREDIANT_LIST_KEY = "ingrediantlist";
 
@@ -26,30 +30,53 @@ public class DetailActivity extends AppCompatActivity implements RecipeDetailFra
     FragmentManager fragmentManager;
     RecipeStepDetailFragment recipeDetailStepFragment;
 
+
+    SharedPreferences shPref;
+    SharedPreferences.Editor shEditor;
+    //***//tosave
+    //getSharedPreferences(SHAREDP_KEY, MODE_PRIVATE).edit().putInt(SHAREDP_INT_KEY, POPselected).apply();
+    //***//toget
+    //getSharedPreferences(SHAREDP_KEY, MODE_PRIVATE).getInt("name", 0);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        getSharedPreferences(Keys.TWO_PANE_SHAREDP, MODE_PRIVATE).edit().putInt(Keys.TWO_PANE_PHONE_OR_TABLET, Keys.PHONE).apply();
         recipeModel = getIntent().getParcelableExtra(RecipesActivity.RECIPE_PARC_KEY);
 
         if (findViewById(R.id.linearLayoutforStepfragment) != null) {
             mTwopane = true;
+            getSharedPreferences(Keys.TWO_PANE_SHAREDP, MODE_PRIVATE).edit().putInt(Keys.TWO_PANE_PHONE_OR_TABLET, Keys.TABLET).apply();
             if (savedInstanceState == null) {
                 setupRecipeDetailsFragment();
                 setupRecipeStepDetailsfragment();
-                Toast.makeText(this, "tablet", Toast.LENGTH_SHORT).show();
+
             }
         } else {
             mTwopane = false;
+            getSharedPreferences(Keys.TWO_PANE_SHAREDP, MODE_PRIVATE).edit().putInt(Keys.TWO_PANE_PHONE_OR_TABLET, Keys.PHONE).apply();
+
             if (savedInstanceState == null) {
                 setupRecipeDetailsFragment();
             }
-            Toast.makeText(this, "phone", Toast.LENGTH_SHORT).show();
+
         }
 
-        Toast.makeText(this, "name is " + recipeModel.getName(), Toast.LENGTH_SHORT).show();
+        int check_phone_or_tablet = getSharedPreferences(Keys.TWO_PANE_SHAREDP, MODE_PRIVATE).getInt(Keys.TWO_PANE_PHONE_OR_TABLET, -1);
 
+        Toast.makeText(this,  checkPhoneOrTablet(check_phone_or_tablet) , Toast.LENGTH_SHORT).show();
+
+    }
+
+    private String checkPhoneOrTablet(int i) {
+        String result = "not specifec Phone or Tablet";
+        if (i == Keys.PHONE) {
+            result = "it's a Phone";
+        } else if (i == Keys.TABLET) {
+            result = "it's a Tablet";
+        }
+        return result;
     }
 
     private void setupRecipeDetailsFragment() {
---
+++ app/src/main/java/com/nd/amrhal/bakingapp/Keys.java
@@ -0,0 +1,12 @@
+package com.nd.amrhal.bakingapp;
+
+public class Keys {
+
+    public static String TWO_PANE_SHAREDP = "mtwopane";
+    public static String TWO_PANE_PHONE_OR_TABLET = "phoneortablet"; //0 for phone //1 for tablet
+    public static int PHONE  =  0  ; //0 for phone case
+    public static int TABLET =  1  ; //0 for tablet case
+
+
+
+}
